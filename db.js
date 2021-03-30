0e616f41cbc35328fb062fd16ff2e5afeb7e9551
ChristianTaidi <christiantaidi@gmail.com>
2021-03-29 22:23:07 -0700

    added redis data load

--- db.js
+++ db.js
@@ -3,4 +3,13 @@ const client = redis.createClient({
     host: '127.0.0.1',
     port: 6379,
     password: ''
-})
+});
+
+client.on('error',err=>{
+    console.log('Error '+ err);
+});
+
+class AppDAO{
+    
+}
+module.exports = AppDAO;
--- index.js
+++ index.js
@@ -1,1 +1,40 @@
- 
+const express = require("express");
+const redis = require('redis');
+
+/**
+ * Redis client init
+ */
+const client = redis.createClient();
+client.on("error", (err)=>{
+    console.log(err);
+});
+
+/**
+ * Web server init
+ */
+const app = express();
+const PORT = process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`Server listening on port ${PORT}`)
+});
+
+/**
+ * GET /bid?position=XXXX&publisherid=XXXX
+ */
+app.get("/bid",async (req, res)=>{
+
+    let position = req.query.position;
+    let publisherId = req.query.publisherid;
+
+    try{
+        client.hgetall("testcampaigns#",(err,res)=>{
+            let data = [];
+            for(value in res){
+                data.push(JSON.parse(res[value]));
+            }
+            console.log(data);
+        })
+    }catch(err){
+        res.status(500).send({message: err.message});
+    }
+})
